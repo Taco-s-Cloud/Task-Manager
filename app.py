6fc884b9915d720f7cbe8acfd63a97658325ad5a
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-11-25 21:43:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from app import create_app
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5003)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5001)))
