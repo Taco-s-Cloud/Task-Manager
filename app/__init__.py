47f3b7ffe1d44c5270b32c8cece737997d43f149
tomasfiure <123346779+tomasfiure@users.noreply.github.com>
2024-12-10 23:18:34 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,27 @@ from flask import Flask
 from .routers import tasks
 from .database import engine, Base
 from flask_cors import CORS
+import uuid
+from google.cloud import logging_v2 as cloud_logging
 
 def create_app():
     app = Flask(__name__)
-    #CORS(app)
-    CORS(app, resources={r"/*": {"origins": "https://taco-ui-1024364663505.us-central1.run.app"}})
+    CORS(app)
+    client = cloud_logging.Client()
+    client.setup_logging() 
+    #Correlation id
+    @app.before_request
+    def set_correlation_id():
+        # Check if the header is present; if not, generate a new UUID
+        correlation_id = request.headers.get("X-Correlation-ID", str(uuid.uuid4()))
+        g.correlation_id = correlation_id
+
+    @app.after_request
+    def add_correlation_id_to_response(response):
+        # Include the correlation ID in the response headers for visibility
+        if hasattr(g, "correlation_id"):
+            response.headers["X-Correlation-ID"] = g.correlation_id
+        return response
     # Register blueprints
     app.register_blueprint(tasks.tasks_blueprint)
 
